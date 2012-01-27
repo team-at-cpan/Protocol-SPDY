23872d694e0707b99d741979b04c9aa0987bc8ca
Tom Molesworth <tom@perlsite.co.uk>
2012-01-27 21:02:58 +0000

    Documentation updates and compilation/test fixes
    git-svn-id: http://entitymodel.com/svn/trunk/Protocol-SPDY@1306 c5bf8c3b-bf20-4ed3-b413-d5547a49f493

--- lib/Protocol/SPDY.pm
+++ lib/Protocol/SPDY.pm
@@ -5,12 +5,76 @@ use warnings FATAL => 'all';
 
 our $VERSION = '0.001';
 
+=head1 NAME
+
+Protocol::SPDY - abstract support for the SPDY protocol
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+These modules aren't much use on their own, since they only deal with the abstract
+protocol. If you want to add SPDY client or server support to your code, you'll need
+a transport as well - try one of these:
+
+=over 4
+
+=item * L<Net::Async::SPDY::Server> - serve SPDY requests using L<IO::Async>
+
+=item * L<Net::Async::SPDY::Client> - connect to SPDY servers using L<IO::Async>
+(although once this is stable support may be added to L<Net::Async::HTTP>,
+see L<https://rt.cpan.org/Ticket/Display.html?id=74387> for progress on this.
+
+=back
+
+Eventually L<POE> or L<AnyEvent> implementations should arrive when someone more
+familiar with those frameworks takes an interest.
+
+For a simple blocking client and server implementation, see the examples/ directory.
+
+=head1 IMPLEMENTATION CONSIDERATIONS
+
+The information in L<http://www.chromium.org/spdy> may be useful when implementing clients
+(browsers).
+
+This abstract protocol class requires a transport implementation.
+
+=head2 UPGRADING EXISTING HTTP OR HTTPS CONNECTIONS
+
+You can inform a browser that SPDY is available through the Alternate-Protocol HTTP
+header:
+
+ Alternate-Protocol: <port>:<protocol>
+
+For example:
+
+ Alternate-Protocol: 2443:spdy/2
+
+This applies both to HTTP and HTTPS.
+
+If the browser is already connected to the server using TLS, the TLS/NPN mechanism can be used
+to indicate that SPDY is available. Currently this requires openssl-1.1 or later,
+although the NPN extension should be simple enough to backport if needed (see
+L<http://www.ietf.org/id/draft-agl-tls-nextprotoneg-00.txt> for details). Since the
+port is already connected, only the <protocol> part is required ('spdy/2' or 'spdy/3')
+when sending via TLS/NPN.
+
+This information could also be provided via the Alternate-Protocol header:
+
+ Alternate-Protocol: 2443:spdy/2,443:npn-spdy/2,443:npn-spdy/3
+
+=cut
+
 # Pull in all the required pieces
 use Protocol::SPDY::Frame;
 use Protocol::SPDY::Frame::Control;
 use Protocol::SPDY::Frame::Data;
 
 use Protocol::SPDY::Constants ':all';;
+
+=head1 METHODS
+
+=cut
 
 =head2 request_close
 
@@ -318,64 +382,6 @@ sub parse_response {
 
 __END__
 
-=head1 NAME
-
-Protocol::SPDY - abstract support for the SPDY protocol
-
-=head1 SYNOPSIS
-
-=head1 DESCRIPTION
-
-These modules aren't much use on their own, since they only deal with the abstract
-protocol. If you want to add SPDY client or server support to your code, you'll need
-a transport as well - try one of these:
-
-=over 4
-
-=item * L<Net::Async::SPDY::Server> - serve SPDY requests using L<IO::Async>
-
-=item * L<Net::Async::SPDY::Client> - connect to SPDY servers using L<IO::Async>
-
-=back
-
-Eventually L<POE> or L<AnyEvent> implementations should arrive when someone more
-familiar with those frameworks takes an interest.
-
-For a simple blocking client and server implementation, see the examples/ directory.
-
-=head1 IMPLEMENTATION CONSIDERATIONS FOR CLIENTS
-
-The information in L<http://www.chromium.org/spdy> may be useful when implementing clients
-(browsers). A simple browser implementation is 
-
-=head1 IMPLEMENTATION CONSIDERATIONS FOR SERVERS
-
-This abstract protocol class requires a transport implementation.
-
-=head2 UPGRADING EXISTING HTTP OR HTTPS CONNECTIONS
-
-You can inform a browser that SPDY is available through the Alternate-Protocol HTTP
-header:
-
- Alternate-Protocol: <port>:<protocol>
-
-For example:
-
- Alternate-Protocol: 2443:spdy/2
-
-This applies both to HTTP and HTTPS.
-
-If the browser is already connected to the server using TLS, the TLS/NPN mechanism can be used
-to indicate that SPDY is available. Currently this requires openssl-1.1 or later,
-although the NPN extension should be simple enough to backport if needed (see
-L<http://www.ietf.org/id/draft-agl-tls-nextprotoneg-00.txt> for details). Since the
-port is already connected, only the <protocol> part is required ('spdy/2' or 'spdy/3')
-when sending via TLS/NPN.
-
-This information could also be provided via the Alternate-Protocol header:
-
- Alternate-Protocol: 2443:spdy/2,443:npn-spdy/2,443:npn-spdy/3
-
 =head1 COMPONENTS
 
 Further documentation can be found in the following modules:
@@ -401,5 +407,5 @@ Tom Molesworth <cpan@entitymodel.com>
 
 =head1 LICENSE
 
-Copyright Tom Molesworth 2011. Licensed under the same terms as Perl itself.
+Copyright Tom Molesworth 2011-2012. Licensed under the same terms as Perl itself.
 
